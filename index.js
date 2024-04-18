07dedbcb3e92261844ba0d007db760634801e71a
H0rangi <formation0451@hotmail.com>
2024-04-18 12:28:10 +0100

    :)

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
 const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api",require("./lib/handler"));
 
